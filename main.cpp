cecbb5116863dfd44668048a7438c68d8fd2b94c
eladkorngut <elad.korngut@mail.huji.ac.il>
2023-03-07 10:13:22 +0200

    Added a GillespieMC function that runs a gillespie time step; instead of doing the time step in the main function the program has a designated function to do that

--- main.cpp
+++ main.cpp
@@ -376,44 +376,22 @@ void inital_networks_stat(int N,double x,int sims,std::list<int>& num_inf,std::l
 }
 
 
-int main() {
-    int m_in,m_out,node;  //k_max maximal degree, N number of nodes, sims number of simulations
-    double death(0),r1,r2,s_m_sum;
-    std::fstream Adjfile_in,Adjfile_out,parametersfile;
-    std::string filename_in("Adjin_0.txt"), filename_out("Adjout_0.txt"), parametersname("parameters.csv");
-    auto parameter_list=read_parameters(parametersname,parametersfile);
-    int N=std::get<0>(parameter_list),sims=std::get<1>(parameter_list),it=std::get<2>(parameter_list),
-                k=std::get<3>(parameter_list),jump=std::get<6>(parameter_list),network_number=std::get<9>(parameter_list),new_trajectory_bin=std::get<12>(parameter_list);
-    double inital_inf_percent=std::get<4>(parameter_list),lam=std::get<5>(parameter_list),Alpha=std::get<7>(parameter_list)
-                ,Beta=std::get<8>(parameter_list),tau=std::get<10>(parameter_list),mf_solution=std::get<11>(parameter_list);
-                std::string dir_path(std::get<13>(parameter_list)),prog=std::get<14>(parameter_list);
-    int steps_c(sims),k_max(0),k_max_out(0); // Need to find ouf which dimension i np.size(n,1) refers to
-    std::list<double> weights(sims,0),wg(sims,0),avec_sum(sims,0),t(sims,0);
-    std::list<double>::iterator it_weights(weights.begin()),it_wg(wg.begin()),it_avec_sum(avec_sum.begin());
-    std::vector<int> degrees_in(N,0),degrees_out(N,0);
-    std::vector<std::vector<int>> Adjlist_in(N,std::vector<int>()),Adjlist_out(N,std::vector<int>());
-    std::list<std::vector<int>> sigma(sims,std::vector<int>(N,0)),infected_node(sims,std::vector<int>()),
-    ng(steps_c,std::vector<int>(N,0)),positions(sims,std::vector<int>(N,0));
-    std::list<std::vector<int>>::iterator it_sigma(sigma.begin()),it_infected_node(infected_node.begin()),it_ng(ng.begin()),it_positions(positions.begin());
-    std::list< std::vector<std::vector<int>>> susceptible_nodes(steps_c,std::vector<std::vector<int>>());
-    std::list< std::vector<std::vector<int>>>::iterator it_susceptible_nodes(susceptible_nodes.begin());
-    std::list<int> num_inf(sims,0),SI(sims,0);
-    std::list<int>::iterator it_num_inf(num_inf.begin()),it_SI(SI.begin());
-    std::list<double>::iterator it_t(t.begin());
-    std::list< std::vector<int>> infected_neighbors_in(steps_c,std::vector<int>(N,0)),infected_neighbors_out(steps_c,std::vector<int>(N,0));
-    std::list< std::vector<int>>::iterator it_infected_neighbors_in(infected_neighbors_in.begin()),it_infected_neighbors_out(infected_neighbors_out.begin());
-
-    //Read the python's network structure, what are the different connections
-
-    read_in_neighborslist(N,filename_in,Adjlist_in,degrees_in, Adjfile_in,k_max);
-    read_in_neighborslist(N,filename_out,Adjlist_out,degrees_out,Adjfile_out,k_max_out);
-
-    // Intailze the different networks list such as the number of infected nodes and so on
-    std::list<std::vector<int>> s_m(steps_c,std::vector<int>(k_max+1,0));
-    std::list<std::vector<int>>::iterator it_s_m(s_m.begin());
-    inital_networks_stat(N,inital_inf_percent,sims,num_inf,infected_node,infected_neighbors_in,infected_neighbors_out,sigma,
-                         degrees_in,degrees_out,Adjlist_in,Adjlist_out,positions,susceptible_nodes,SI,s_m,k_max);
+void GillespieMC(int steps_c,std::list<int> &num_inf,std::list<int>::iterator &it_num_inf,
+                 std::list<double>::iterator &it_avec_sum,std::list<double> &avec_sum,
+                 std::list<double>::iterator &it_t,std::list<double> &t,
+                 std::list<std::vector<int>> &infected_node,std::list<std::vector<int>>::iterator &it_infected_node,
+                 std::list< std::vector<int>> &infected_neighbors_in,std::list< std::vector<int>>::iterator &it_infected_neighbors_in,
+                 std::list< std::vector<int>> &infected_neighbors_out,std::list< std::vector<int>>::iterator &it_infected_neighbors_out,
+                 std::list<std::vector<int>> &sigma,std::list<std::vector<int>>::iterator &it_sigma,
+                 std::list<std::vector<int>> &s_m,std::list<std::vector<int>>::iterator &it_s_m,
+                 std::list<std::vector<int>> &positions,std::list<std::vector<int>>::iterator &it_positions,
+                 std::list< std::vector<std::vector<int>>> &susceptible_nodes,std::list< std::vector<std::vector<int>>>::iterator& it_susceptible_nodes,
+                 std::list<int> &SI,std::list<int>::iterator &it_SI,std::list<bool> &skip,std::list<bool>::iterator &it_skip,
+                 double Alpha,double tau,int k_max,double death,double Beta,std::vector<int> &degrees_in,std::vector<int> &degrees_out,
+                 std::vector<std::vector<int>> &Adjlist_in,std::vector<std::vector<int>> &Adjlist_out,std::list<double>::iterator &it_weights){
     int temp_count_steps(0),temp_count_num_inf(0);
+    int m_in,m_out,node;
+    double r1,r2,s_m_sum;
 
     while (0<steps_c){
         //The initialized the iterators of the different list
@@ -431,15 +409,35 @@ int main() {
         it_positions=positions.begin();
         it_susceptible_nodes =susceptible_nodes.begin();
         it_SI = SI.begin();
+        it_skip = skip.begin();
 
 
         // For all networks draw the exponentially distributed time step
         while (it_num_inf!=num_inf.end()){
+            if (*it_skip){
+                ++it_num_inf;
+                ++it_avec_sum;
+                ++it_t;
+                ++it_infected_node;
+                ++it_infected_neighbors_in;
+                ++it_infected_neighbors_out;
+                ++it_sigma;
+                ++it_s_m;
+                ++it_positions;
+                ++it_susceptible_nodes;
+                ++it_SI;
+                ++it_skip;
+                continue;
+            }
             temp_count_num_inf++;
             r1 = rand()/(double(RAND_MAX));
             r2 = std::log(1.0/(double(rand())/double(RAND_MAX)));
             *it_avec_sum = (*it_num_inf)*Alpha+(*it_SI)*Beta;
             *it_t = *it_t + r2/(*it_avec_sum);
+            if (*it_t>tau){
+                (*it_skip)=true;
+                steps_c =steps_c-1;
+            }
 //            *it_t =*(std::prev(it_t)) + r2/(*it_avec_sum);
 
             //Pick a node, change its state and update the transition rates
@@ -545,6 +543,7 @@ int main() {
             ++it_positions;
             ++it_susceptible_nodes;
             ++it_SI;
+            ++it_skip;
         } //end one time step
 
         temp_count_num_inf=0;
@@ -564,7 +563,7 @@ int main() {
         while (it_num_inf!=num_inf.end()){
             temp_count_num_inf++;
             if (*it_num_inf<=0){
-                 death = death + (*it_weights);
+                death = death + (*it_weights);
                 it_num_inf=num_inf.erase(it_num_inf);
                 it_avec_sum=avec_sum.erase(it_avec_sum);
                 it_t=t.erase(it_t);
@@ -593,10 +592,242 @@ int main() {
             ++it_positions;
             ++it_susceptible_nodes;
             ++it_SI;
+            ++it_skip;
         }
         std::cout <<"Time "<<  temp_count_steps <<" "<< "Simulation "<<temp_count_num_inf<<std::endl;
         temp_count_num_inf=0;
     }
+}
+
+
+int main() {
+    int m_in,m_out,node;  //k_max maximal degree, N number of nodes, sims number of simulations
+    double death(0),r1,r2,s_m_sum;
+    std::fstream Adjfile_in,Adjfile_out,parametersfile;
+    std::string filename_in("Adjin_0.txt"), filename_out("Adjout_0.txt"), parametersname("parameters.csv");
+    auto parameter_list=read_parameters(parametersname,parametersfile);
+    int N=std::get<0>(parameter_list),sims=std::get<1>(parameter_list),it=std::get<2>(parameter_list),
+                k=std::get<3>(parameter_list),jump=std::get<6>(parameter_list),network_number=std::get<9>(parameter_list),new_trajectory_bin=std::get<12>(parameter_list);
+    double inital_inf_percent=std::get<4>(parameter_list),lam=std::get<5>(parameter_list),Alpha=std::get<7>(parameter_list)
+                ,Beta=std::get<8>(parameter_list),tau=std::get<10>(parameter_list),mf_solution=std::get<11>(parameter_list);
+                std::string dir_path(std::get<13>(parameter_list)),prog=std::get<14>(parameter_list);
+    int steps_c(sims),k_max(0),k_max_out(0); // Need to find ouf which dimension i np.size(n,1) refers to
+    std::list<double> weights(sims,1.0/double(sims)),wg(sims,1.0/double(sims)),avec_sum(sims,0),t(sims,0);
+    std::list<double>::iterator it_weights(weights.begin()),it_wg(wg.begin()),it_avec_sum(avec_sum.begin());
+    std::vector<int> degrees_in(N,0),degrees_out(N,0);
+    std::vector<std::vector<int>> Adjlist_in(N,std::vector<int>()),Adjlist_out(N,std::vector<int>());
+    std::list<std::vector<int>> sigma(sims,std::vector<int>(N,0)),infected_node(sims,std::vector<int>()),
+    ng(steps_c,std::vector<int>(N,0)),positions(sims,std::vector<int>(N,0));
+    std::list<std::vector<int>>::iterator it_sigma(sigma.begin()),it_infected_node(infected_node.begin()),it_ng(ng.begin()),it_positions(positions.begin());
+    std::list< std::vector<std::vector<int>>> susceptible_nodes(steps_c,std::vector<std::vector<int>>());
+    std::list< std::vector<std::vector<int>>>::iterator it_susceptible_nodes(susceptible_nodes.begin());
+    std::list<int> num_inf(sims,0),SI(sims,0);
+    std::list<int>::iterator it_num_inf(num_inf.begin()),it_SI(SI.begin());
+    std::list<double>::iterator it_t(t.begin());
+    std::list< std::vector<int>> infected_neighbors_in(steps_c,std::vector<int>(N,0)),infected_neighbors_out(steps_c,std::vector<int>(N,0));
+    std::list< std::vector<int>>::iterator it_infected_neighbors_in(infected_neighbors_in.begin()),it_infected_neighbors_out(infected_neighbors_out.begin());
+    std::list<bool> skip(sims, false);
+    std::list<bool>::iterator it_skip=skip.begin();
+
+
+    //Read the python's network structure, what are the different connections
+
+    read_in_neighborslist(N,filename_in,Adjlist_in,degrees_in, Adjfile_in,k_max);
+    read_in_neighborslist(N,filename_out,Adjlist_out,degrees_out,Adjfile_out,k_max_out);
+
+    // Intailze the different networks list such as the number of infected nodes and so on
+    std::list<std::vector<int>> s_m(steps_c,std::vector<int>(k_max+1,0));
+    std::list<std::vector<int>>::iterator it_s_m(s_m.begin());
+    inital_networks_stat(N,inital_inf_percent,sims,num_inf,infected_node,infected_neighbors_in,infected_neighbors_out,sigma,
+                         degrees_in,degrees_out,Adjlist_in,Adjlist_out,positions,susceptible_nodes,SI,s_m,k_max);
+    int temp_count_steps(0),temp_count_num_inf(0);
+    
+    GillespieMC(steps_c,num_inf,it_num_inf,it_avec_sum,avec_sum,it_t,t,infected_node,it_infected_node,infected_neighbors_in,it_infected_neighbors_in,
+            infected_neighbors_out,it_infected_neighbors_out,sigma,it_sigma,s_m,it_s_m,positions,it_positions,susceptible_nodes,it_susceptible_nodes,
+            SI,it_SI,skip,it_skip,Alpha,tau,k_max,death,Beta,degrees_in,degrees_out,Adjlist_in,Adjlist_out,it_weights);
+
+//    while (0<steps_c){
+//        //The initialized the iterators of the different list
+//        temp_count_steps++;
+//        temp_count_num_inf=0;
+//
+//        it_num_inf=num_inf.begin();
+//        it_avec_sum=avec_sum.begin();
+//        it_t=t.begin();
+//        it_infected_node=infected_node.begin();
+//        it_infected_neighbors_in=infected_neighbors_in.begin();
+//        it_infected_neighbors_out=infected_neighbors_out.begin();
+//        it_sigma=sigma.begin();
+//        it_s_m=s_m.begin();
+//        it_positions=positions.begin();
+//        it_susceptible_nodes =susceptible_nodes.begin();
+//        it_SI = SI.begin();
+//
+//
+//        // For all networks draw the exponentially distributed time step
+//        while (it_num_inf!=num_inf.end()){
+//            temp_count_num_inf++;
+//            r1 = rand()/(double(RAND_MAX));
+//            r2 = std::log(1.0/(double(rand())/double(RAND_MAX)));
+//            *it_avec_sum = (*it_num_inf)*Alpha+(*it_SI)*Beta;
+//            *it_t = *it_t + r2/(*it_avec_sum);
+////            *it_t =*(std::prev(it_t)) + r2/(*it_avec_sum);
+//
+//            //Pick a node, change its state and update the transition rates
+//            if (r1<(*it_num_inf)*Alpha/(*it_avec_sum)) {
+//                // Recover an infected node
+//                node = (*it_infected_node)[int(rand())%(*it_num_inf)]; //randomly choose infected node
+////                m = (*it_infected_neighbors)[node]; //number of infected neighbors
+//                m_in = (*it_infected_neighbors_in)[node]; //number of infected neighbors that infecting the node
+//                m_out = (*it_infected_neighbors_out)[node]; //number of infected neighbors that the node infect
+//                (*it_sigma)[node]=0;
+//                (*it_num_inf)--;
+//                //Remove node from infected list and add to susceptible list
+//                remove_infected_node(node,*it_infected_node,*it_positions);
+//                if ((*it_num_inf)==0){
+//                    //Advance to the next simulation in the list
+//                    ++it_num_inf;
+//                    ++it_avec_sum;
+//                    ++it_t;
+//                    ++it_infected_node;
+//                    ++it_infected_neighbors_in;
+//                    ++it_infected_neighbors_out;
+//                    ++it_sigma;
+//                    ++it_s_m;
+//                    ++it_positions;
+//                    ++it_susceptible_nodes;
+//                    ++it_SI;
+//                    continue;
+//                }
+////                (*it_SI) = 2*m-degrees[node];
+//                (*it_SI) = (*it_SI) + m_in + m_out - degrees_out[node];
+//
+////                remove_infected_node(node,*it_infected_node,*it_positions);
+////                add_susceptible_node(node,m,*it_susceptible_nodes,*it_positions);
+////                if ((*it_num_inf)<=0){
+////                    continue;
+////                }
+//                add_susceptible_node(node,m_in,*it_susceptible_nodes,*it_positions);
+//
+//
+//                // Increment number of susceptible nodes with m infected nodes
+//                (*it_s_m)[m_in]++;
+//
+//
+//                decrement_susc_neighbs(degrees_in[node],degrees_out[node],Adjlist_in[node],Adjlist_out[node],*it_susceptible_nodes,
+//                                       *it_infected_neighbors_in,*it_infected_neighbors_out,*it_s_m,*it_positions,*it_sigma,node,*it_infected_node,temp_count_steps,temp_count_num_inf);
+//
+//
+//            }
+//            else{
+//                //Infected a susceptible node
+//                // Pick an m class with probability proportional to the total number of SI links in that m class i.e. s_m[m]*m
+//                r1= rand()/(double(RAND_MAX));
+//                r1=r1*(*it_SI);
+//                s_m_sum = 0;
+////                for (int m1=0;m1<=k_max;m1++){
+//                for (int m1=1;m1<=k_max;m1++){
+//                    s_m_sum = s_m_sum + (*it_s_m)[m1]*m1;
+//                    if (r1<=s_m_sum){
+//                        // choose a node with m infected neighbours at random
+//                        node = (*it_susceptible_nodes)[m1][rand()%(*it_s_m)[m1]];
+//                        break;
+//                    }
+//                }
+////                m = (*it_infected_neighbors)[node];
+//                m_in = (*it_infected_neighbors_in)[node];
+//                m_out = (*it_infected_neighbors_out)[node];
+//                (*it_sigma)[node] = 1;
+//                *it_num_inf = (*it_num_inf)+1;
+////                *it_SI = *it_SI - 2*m - degrees[node];
+////                (*it_SI) = 2*m_in+2*m_out-degrees_in[node]-degrees_out[node];
+//
+//
+//                // Remove the node from susceptible list and add to infected list
+//                remove_susceptible_node(node,m_in,*it_susceptible_nodes,*it_positions);
+//                add_infected_node(node,*it_infected_node,*it_positions);
+//
+//                //Decrement number of susceptible nodes
+//                (*it_s_m)[m_in]--;
+//
+//                // change the neighbours to other lists and adjust s_m
+//
+////                increment_susc_neighbs(degrees_in[node],degrees_out[node],Adjlist_in[node],Adjlist_out[node],
+////                                       *it_susceptible_nodes,*it_infected_neighbors_in,*it_infected_neighbors_out,
+////                                       *it_s_m,*it_positions,*it_sigma);
+////                if (susceptible_nodes.size()<198){
+////                    bool stop= true;
+////                }
+//                increment_susc_neighbs(degrees_in[node],degrees_out[node],Adjlist_in[node],Adjlist_out[node],
+//                                       *it_susceptible_nodes,*it_infected_neighbors_in,*it_infected_neighbors_out,
+//                                       *it_s_m,*it_positions,*it_sigma);
+//
+//                (*it_SI) = (*it_SI) + degrees_out[node] - m_in - m_out;
+//            }
+//            //Advance to the next simulation in the list
+//            ++it_num_inf;
+//            ++it_avec_sum;
+//            ++it_t;
+//            ++it_infected_node;
+//            ++it_infected_neighbors_in;
+//            ++it_infected_neighbors_out;
+//            ++it_sigma;
+//            ++it_s_m;
+//            ++it_positions;
+//            ++it_susceptible_nodes;
+//            ++it_SI;
+//        } //end one time step
+//
+//        temp_count_num_inf=0;
+//
+//        it_num_inf=num_inf.begin();
+//        it_avec_sum=avec_sum.begin();
+//        it_t=t.begin();
+//        it_infected_node=infected_node.begin();
+//        it_infected_neighbors_in=infected_neighbors_in.begin();
+//        it_infected_neighbors_out=infected_neighbors_out.begin();
+//        it_sigma=sigma.begin();
+//        it_s_m=s_m.begin();
+//        it_positions=positions.begin();
+//        it_susceptible_nodes =susceptible_nodes.begin();
+//        it_SI = SI.begin();
+//
+//        while (it_num_inf!=num_inf.end()){
+//            temp_count_num_inf++;
+//            if (*it_num_inf<=0){
+//                 death = death + (*it_weights);
+//                it_num_inf=num_inf.erase(it_num_inf);
+//                it_avec_sum=avec_sum.erase(it_avec_sum);
+//                it_t=t.erase(it_t);
+//                it_infected_node=infected_node.erase(it_infected_node);
+//                it_infected_neighbors_in=infected_neighbors_in.erase(it_infected_neighbors_in);
+//                it_infected_neighbors_out=infected_neighbors_out.erase(it_infected_neighbors_out);
+//                it_sigma=sigma.erase(it_sigma);
+//                it_s_m=s_m.erase(it_s_m);
+//                it_positions=positions.erase(it_positions);
+//                it_susceptible_nodes=susceptible_nodes.erase(it_susceptible_nodes);
+//                it_SI=SI.erase(it_SI);
+//                steps_c =steps_c-1;
+//                if (it_num_inf==num_inf.end()){
+//                    break;
+//                }
+//                continue;
+//            }
+//            ++it_num_inf;
+//            ++it_avec_sum;
+//            ++it_t;
+//            ++it_infected_node;
+//            ++it_infected_neighbors_in;
+//            ++it_infected_neighbors_out;
+//            ++it_sigma;
+//            ++it_s_m;
+//            ++it_positions;
+//            ++it_susceptible_nodes;
+//            ++it_SI;
+//        }
+//        std::cout <<"Time "<<  temp_count_steps <<" "<< "Simulation "<<temp_count_num_inf<<std::endl;
+//        temp_count_num_inf=0;
+//    }
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
