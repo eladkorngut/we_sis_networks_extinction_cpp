Added a method to create a graph without nodes that have degree zero. Now, I find the average degree using a binary search and the program does not go into an infinte loop. Also there was a bug in the error_graph, True instead of false

rand_networks.py
@@ -891,39 +891,44 @@ def find_multi_k_binary_search(kavg,epsilon,n,net_type):
 
 
 def configuration_model_undirected_graph_mulit_type(kavg,epsilon,N,net_type):
-    k_avg_graph = 0
+    k_avg_graph,kavg_graph_min,kavg_graph_max = 0,0.0,kavg
     if N>1000:
         while np.abs(kavg-k_avg_graph)/kavg>0.05:
+            kavg_mid = (kavg_graph_max+kavg_graph_min)/2
             if net_type=='ig':
-                wald_mu, wald_lambda = kavg, kavg / epsilon ** 2
+                wald_mu, wald_lambda = kavg_mid, kavg_mid / epsilon ** 2
                 d = numpy.random.default_rng().wald(wald_mu,wald_lambda,N).astype(int)
             elif net_type=='bet':
-                alpha_beta_dist,beta_beta_dist = (N - kavg * (1 + epsilon ** 2)) / (N * epsilon ** 2),((kavg - N) * (kavg - N + kavg * epsilon ** 2)) / (kavg * N * epsilon ** 2)
+                alpha_beta_dist,beta_beta_dist = (N - kavg_mid * (1 + epsilon ** 2)) / (N * epsilon ** 2),((kavg_mid - N) * (kavg_mid - N + kavg_mid * epsilon ** 2)) / (kavg_mid * N * epsilon ** 2)
                 d=(numpy.random.default_rng().beta(alpha_beta_dist,beta_beta_dist,N)*N).astype(int)
             elif net_type=='ln':
-                mu_log_norm,sigma_log_norm = -(1 / 2) * np.log((1 + epsilon ** 2) / kavg ** 2),np.sqrt(2 * np.log(kavg) + np.log((1 + epsilon ** 2) / kavg ** 2))
+                mu_log_norm,sigma_log_norm = -(1 / 2) * np.log((1 + epsilon ** 2) / kavg_mid ** 2),np.sqrt(2 * np.log(kavg_mid) + np.log((1 + epsilon ** 2) / kavg_mid ** 2))
                 d = numpy.random.lognormal(mu_log_norm,sigma_log_norm,N).astype(int)
             elif net_type=='gam':
-                theta, shape, k_avg_graph = epsilon ** 2 * kavg, 1 / epsilon ** 2, 0.0
+                theta, shape, k_avg_graph = epsilon ** 2 * kavg_mid, 1 / epsilon ** 2, 0.0
                 d = numpy.random.default_rng().gamma(shape, theta, N).astype(int)
-            # # Remove zeros from d
-            # d = d[d != 0]
+            # Remove zeros from d
+            d = d[d != 0]
             if np.sum(d)%2!=0:
                 d[int(len(d)*np.random.random())]+=1
             G = nx.configuration_model(d)
             G = nx.Graph(G)
             G.remove_edges_from(nx.selfloop_edges(G))
-            k_avg_graph = np.mean([G.degree(n) for n in G.nodes()])
-            graph_degrees = np.array([G.degree(n) for n in G.nodes()])
-            # Remove nodes with zero degree
-            zero_degree_nodes = [n for n, d in G.degree() if d == 0]
-            G.remove_nodes_from(zero_degree_nodes)
-        return G,graph_degrees
+            graph_degrees= np.array([G.degree(n) for n in G.nodes()])
+            k_avg_graph = np.mean(graph_degrees)
+            if k_avg_graph<kavg:
+                kavg_graph_min = k_avg_graph
+            else:
+                kavg_graph_max = k_avg_graph
+            # # Remove nodes with zero degree
+            # zero_degree_nodes = [n for n, d in G.degree() if d == 0]
+            # G.remove_nodes_from(zero_degree_nodes)
+        return G,np.array([G.degree(n) for n in G.nodes()])
     G,kavg_graph = find_multi_k_binary_search(kavg,epsilon,N,net_type)
     graph_degrees = np.array([G.degree(n) for n in G.nodes()])
-    # Remove nodes with zero degree
-    zero_degree_nodes = [n for n, d in G.degree() if d == 0]
-    G.remove_nodes_from(zero_degree_nodes)
+    # # Remove nodes with zero degree
+    # zero_degree_nodes = [n for n, d in G.degree() if d == 0]
+    # G.remove_nodes_from(zero_degree_nodes)
 
     return G,graph_degrees
 
@@ -1058,9 +1063,9 @@ if __name__ == '__main__':
     # class CustomDistribution(rv_discrete):
     #     def _pmf(self, k, a, b):
     #         return b * a / (1 + b * k) ** (a + 1)
-    k,epsilon,N,net_type= 10,3.0,10000,'gam'
+    k,epsilon,N,net_type= 20,1.5,10000,'gam'
     # G = configuration_model_undirected_graph_gamma(k,epsilon,N)
-    G = configuration_model_undirected_graph_mulit_type(k,epsilon,N,net_type)
+    G,degree_sequence = configuration_model_undirected_graph_mulit_type(k,epsilon,N,net_type)
     plot_gamma_distribution(G,k,epsilon,N,net_type)
     # custom_dist = CustomDistribution()
     # a,k,n=10.0,20,10000

runwesim.py
@@ -258,7 +258,7 @@ def job_to_cluster(foldername,parameters,Istar,prog):
                                    dir_path,prog,dir_path,eps_graph,eps_graph,graph_std,graph_skewness,third_moment,second_moment])
             np.save('parameters_{}.npy'.format(i), parameters)
         else:
-            if error_graphs==False:
+            if error_graphs==True:
                 G,graph_degrees = rand_networks.configuration_model_undirected_graph_mulit_type(float(k),float(eps_din),int(N),prog)
                 # graph_degrees = np.array([G.degree(n) for n in G.nodes()])
                 k_avg_graph, graph_std, graph_skewness = np.mean(graph_degrees), np.std(graph_degrees), skew(
@@ -288,26 +288,26 @@ def job_to_cluster(foldername,parameters,Istar,prog):
 
 if __name__ == '__main__':
     # Parameters for the network to work
-    N = 100000 # number of nodes
+    N = 10000 # number of nodes
     lam = 1.3 # The reproduction number
     number_of_networks = 20
     sims = 1000 # Number of simulations at each step
     # k = N # Average number of neighbors for each node
-    k = 10 # Average number of neighbors for each node
+    k = 20 # Average number of neighbors for each node
     x = 0.2 # intial infection percentage
     Num_inf = int(x*N) # Number of initially infected nodes
     it = 70
     jump = 1
     Alpha = 1.0 # Recovery rate
     Beta_avg = Alpha * lam / k # Infection rate for each node
-    eps_din,eps_dout = 3.0,3.0 # The normalized std (second moment divided by the first) of the network
+    eps_din,eps_dout = 1.5,1.5 # The normalized std (second moment divided by the first) of the network
     a = 0.2
     # G = nx.random_regular_graph(k,N) # Creates a random graphs with k number of neighbors
     relaxation_time  = 20
     # tau = 1/(Num_inf*Alpha+N*Beta*k)
-    tau = 0.6
+    tau = 1.0
     new_trajcetory_bin = 2
-    prog = 'gam'
+    prog = 'ig'
     error_graphs = False
     parameters = np.array([N,sims,it,k,x,lam,jump,Num_inf,Alpha,number_of_networks,tau,eps_din,eps_dout,new_trajcetory_bin,prog,Beta_avg,error_graphs])
     # parameters = np.array([N, sims, it, k, x, lam, jump, Num_inf, Alpha, number_of_networks, tau, a, new_trajcetory_bin,